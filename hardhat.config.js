8417d28b3313ea2b41a0bca1962d84a6f7b68973
CyAaron Tai Nava <cyaaron.tai.nava@gmail.com>
2022-11-06 16:23:51 -0600

    readme

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,10 +1,17 @@
 require('@nomicfoundation/hardhat-toolbox');
 const Config = require('./utils/evm/Config');
-const fs = require('fs');
 
+const fs = require('fs');
 if (!fs.existsSync(`./utils/deploymentMap`))
   fs.mkdirSync('./utils/deploymentMap');
 if (!fs.existsSync(`./utils/interfaces`)) fs.mkdirSync('./utils/interfaces');
+if (!fs.existsSync(`.env`)) {
+  fs.writeFileSync('.env', fs.readFileSync('sample.env', 'utf-8'));
+  console.log(
+    '\n\x1B[31mENV FILE NOT DETECTED\x1B[39m\n\x1B[33mOne has been generated for your convenience. Please try that again!\x1B[39m\n'
+  );
+  process.exit(1);
+}
 
 // Config(
 //   printToConsole = false,
