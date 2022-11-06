1b4ae6b3ab8e194cd90cf00067f807650313dc3c
CyAaron Tai Nava <cyaaron.tai.nava@gmail.com>
2022-11-06 15:41:06 -0600

    live test

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -3,21 +3,33 @@ const hre = require('hardhat');
 const deployment = require('../utils/evm/deployment');
 const contract = require('../utils/evm/contract');
 
-async function deployLock() {
-  const unlockTime = Math.round(Date.now() / 1000) + 365 * 24 * 60 * 60;
-  const value = hre.ethers.utils.parseUnits('1.0', 'gwei');
+async function deploy(chainId, unlockTime, value) {
   return await deployment.andVerify('Lock', chainId, [unlockTime, { value }]);
 }
 
-async function checkLock() {
+async function andValidate(chainId, unlockTime, value) {
   const lock = contract.retrieve('Lock', chainId);
-  const signers = await hre.ethers.provider.getSigners();
+  const signers = await hre.ethers.getSigners();
+  let result;
+
+  result = parseInt((await lock.unlockTime()).toString());
+  if (result !== unlockTime) throw new Error('unlockTime');
+
+  result = await lock.owner();
+  if (result !== signers[0].address) throw new Error('owner');
+
+  result = parseInt(
+    (await hre.ethers.provider.getBalance(lock.address)).toString()
+  );
+  if (result !== parseInt(value.toString())) throw new Error('value');
 }
 
 (async () => {
   const { chainId } = await hre.ethers.provider.getNetwork();
-  await deployLock(chainId);
-  await checkLock(chainId);
+  const unlockTime = Math.round(Date.now() / 1000) + 365 * 24 * 60 * 60;
+  const value = hre.ethers.utils.parseUnits('1.0', 'gwei');
+  await deploy(chainId, unlockTime, value);
+  await andValidate(chainId, unlockTime, value);
 })().catch((error) => {
   console.error(error);
   process.exitCode = 1;
